f037411e92600b55281026cbf5afd2cc6f2b385e
Paulo.Azevedo <pauloemidioazevedo@gmail.com>
2022-10-17 13:56:29 -0300

    story

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,11 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  viteFinal: (config , { configType}) => {
+    if(configtype === "PRODUCTION"){
+      config.base = "/ignite-storybook/"
+    }
+    return config
+  },
 }
